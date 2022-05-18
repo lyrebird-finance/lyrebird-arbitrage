a263ca57b0e6e0f89b19b40540d6278568d4ed78
Anh Mac <anh@bespokify.com>
2022-05-18 14:40:18 +0700

    feat(sentry): integrate sentry at the warning log level

--- src/utils/loggingUtils.ts
+++ src/utils/loggingUtils.ts
@@ -1,22 +1,39 @@
-import { pino } from 'pino';
+import { DestinationStream, LoggerOptions, multistream, pino } from 'pino';
+import { createWriteStream } from 'pino-sentry';
 import { config } from '../config';
+import PinoPretty from 'pino-pretty';
 
 const properties = config.getProperties();
 
+// - standard output with pretty
+const pinoPrettyStream = PinoPretty({
+  colorize: false,
+  levelFirst: true,
+  ignore: 'pid,hostname',
+  translateTime: 'yyyy-dd-mm HH:MM:ss',
+});
+
+// - sentry output without pretty
+const WITH_SENTRY: boolean = properties.withSentry;
+const SENTRY_DSN: string = properties.sentryDsn;
+
+const sentryStream = WITH_SENTRY
+  ? createWriteStream({ dsn: SENTRY_DSN, level: 'warning' })
+  : undefined;
+
+// - streams 
+const streams = [pinoPrettyStream, sentryStream as DestinationStream]
+  .filter(stream => stream !== undefined)
+  .map(stream => ({ stream }));
+
+// - logger
 const NODE_ENV: string = properties.env;
 
-const logger = pino({
-  level: NODE_ENV === 'prod' ? 'info' : 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: false,
-      levelFirst: true,
-      ignore: 'pid,hostname',
-      translateTime: 'yyyy-dd-mm HH:MM:ss',
-    },
-  },
-});
+const loggerOpts: LoggerOptions = {
+  level: NODE_ENV === 'prod' ? 'info' : 'debug'
+};
+
+const logger = pino(loggerOpts, multistream(streams));
 
 // eslint-disable-next-line import/prefer-default-export
 export { logger };
